6eed06b4f5d19ba4fd9fd366e9c73f38755507e4
Raymond Rabago <mba@12211238-9671W.local>
2025-03-20 13:07:18 +0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// function that will return if the value is not null or undefined
+const isNotNullOrUndefined = (value) => {
+    return value !== null && value !== undefined && value !== 'undefined';
+}
+
+
+const Main = {
+
+    init: () => {
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    Main.init();
+})
